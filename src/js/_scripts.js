f9079d4b6a174bab8f0e295c065065152dc50704
WebFlat <y.vacheslav@gmail.com>
2019-12-12 01:11:56 +0200

    upload-3

--- src/js/_scripts.js
+++ src/js/_scripts.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
 	'use strict';
+
+
+
 	$('.owl-carousel').owlCarousel({
 		loop:true,
 		items:1,
 		autoplay: true,
 		smartSpeed: 1000
 	});
+
+
+	$('#btn-about').click(function(e) {
+		e.preventDefault();
+		$('#home-block').css('display','none');
+		$('#about-block').fadeTo(200,0.1,function() { 
+             $(this).css('display','grid').fadeTo(900,1);
+            });
+	});
+	$('#btn-home').click(function(e) {
+		e.preventDefault();
+		$('#about-block').css('display','none');
+		$('#home-block').fadeTo(200,0.1,function() { 
+             $(this).css('display','block').fadeTo(900,1);
+            });
+	});
+
+
+	
 });
